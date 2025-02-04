d9955f8340d925ad4349fed17e75e225b89d8196
Stock Archery <stockarchery.bihar@gmail.com>
2025-02-04 17:46:52 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ from apscheduler.schedulers.background import BackgroundScheduler
 app = Flask(__name__)
 
 # Enable CORS for all origins
-CORS(app)
+CORS(app, resources={r"/stocks": {"origins": "https://gleaming-lokum-2106f6.netlify.app"}}
 
 # List of all stocks
 all_stocks = [
