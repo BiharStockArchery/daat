13cd2cb2e4a3ea9e0ae0d59c6d8e80cdf9794ed6
Stock Archery <stockarchery.bihar@gmail.com>
2025-02-05 10:40:19 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ from apscheduler.schedulers.background import BackgroundScheduler
 app = Flask(__name__)
 
 
-CORS(app, resources={r"/stocks": {"origins": "https://gleaming-lokum-2106f6.netlify.app"}})
+CORS(app, resources={r"/stocks": {"origins": "https://gleaming-lokum-2106f6.netlify.app/home"}})
 # List of all stocks
 all_stocks = [
     "AXISBANK.NS", "AUBANK.NS", "BANDHANBNK.NS", "BANKBARODA.NS", "BANKINDIA.NS",
